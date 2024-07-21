5a67176a1ab50ae1ffcdf5cb6d0aa7869b56b277
krishna9809 <multiatoms980996@gmail.com>
2024-07-21 14:57:07 +0530

    django

---
+++ cool/templatetags/custom_filters.py
@@ -0,0 +1,20 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='multiply')
+
+
+def multiply(value, arg):
+    """Multiplies the value by the argument."""
+    try:
+        return value * arg
+    except TypeError:
+        return value
+
+@register.filter(name='to_upper')
+def to_upper(value):
+    """Converts a string to uppercase."""
+    if isinstance(value, str):
+        return value.upper()
+    return value
