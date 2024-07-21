793bcfb860c4cb38c41ffe457ae153c6271ccb2d
krishna9809 <multiatoms980996@gmail.com>
2024-07-21 15:21:40 +0530

    CustomFilters

--- letsDoIt/urls.py
+++ letsDoIt/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from cool.views import *
+from custom.views import *
 
 urlpatterns = [
 
     path('', home),
+    path('custom/', Filter),
     path('swag/',swag),
     path('swag/me/',swagme),
     path('swag/me/and/you/',swagmeandyou),
